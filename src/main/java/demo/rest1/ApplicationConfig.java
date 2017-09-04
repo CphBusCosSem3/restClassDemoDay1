d45a936f1dbfaae0fae129cae27bed237d871dad
Lars Mortensen (LAM - Adjunkt - Cphbusiness) <lam@cphbusiness.dk>
2017-09-04 15:51:40 +0200

    what we did day 1

---
+++ src/main/java/demo/rest1/ApplicationConfig.java
@@ -0,0 +1,23 @@
+package demo.rest1;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+
+@javax.ws.rs.ApplicationPath("api")
+public class ApplicationConfig extends Application {
+
+  @Override
+  public Set<Class<?>> getClasses() {
+    Set<Class<?>> resources = new java.util.HashSet<>();
+    addRestResourceClasses(resources);
+    return resources;
+  }
+
+  
+  private void addRestResourceClasses(Set<Class<?>> resources) {
+    resources.add(demo.rest1.PersonResource.class);
+    resources.add(demo.rest1.TestResource.class);
+  }
+
+}
---
+++ src/main/java/jsonmappers/Person.java
@@ -0,0 +1,49 @@
+package jsonmappers;
+
+
+public class Person {
+  private String fName;
+  private String lName;
+  private String phone;
+  private int id;
+
+  public String getfName() {
+    return fName;
+  }
+
+  public void setfName(String fName) {
+    this.fName = fName;
+  }
+
+  public String getlName() {
+    return lName;
+  }
+
+  public void setlName(String lName) {
+    this.lName = lName;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public Person(int id, String fName, String lName, String phone) {
+    this.fName = fName;
+    this.lName = lName;
+    this.phone = phone;
+    this.id = id;
+  }
+  
+}
