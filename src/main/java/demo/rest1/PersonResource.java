ce2e6a6555b1059bcc0bfb783d01b8607322522a
Bladtman <sigurt.bladt@gmail.com>
2017-09-05 15:06:08 +0200

    "Finnished" demo on day 2

--- src/main/java/demo/rest1/PersonResource.java
+++ src/main/java/demo/rest1/PersonResource.java
@@ -1,4 +1,3 @@
-
 package demo.rest1;
 
 import com.google.gson.Gson;
@@ -8,6 +7,7 @@ import java.util.Map;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -20,46 +20,66 @@ import jsonmappers.Person;
 @Path("person")
 public class PersonResource {
 
-  @Context
-  private UriInfo context;
+    @Context
+    private UriInfo context;
 
-  
-  private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
-  private static Map<Integer,Person> persons = new HashMap();
-  private static int nextId = 0;
+    private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-  public PersonResource() {
-    if(persons.isEmpty()){
-      persons.put(nextId, new Person(nextId,"Kurt","Wonnegut","2345"));
-      nextId++;
+    /**
+     * Note; we use simple static variables to persist the person resource, and to keep track of the next id.
+     * This is fine for our example purposes, but is a concurrency nightmare in practice.
+     * DO NOT DO THIS
+     */
+    private static Map<Integer, Person> persons = new HashMap();
+    private static int nextId = 0;
+
+    public PersonResource() {
+        if (persons.isEmpty()) {
+            persons.put(nextId, new Person(nextId, "Kurt", "Wonnegut", "2345"));
+            nextId++;
+        }
     }
-  }
 
-  @GET
-  @Path("all")
-  @Produces(MediaType.APPLICATION_JSON)
-  public String getAll() {
-    return gson.toJson(persons.values());
-  }
-  
-  @GET
-  @Path("{id}")
-  @Produces(MediaType.APPLICATION_JSON)
-  public String getJson(@PathParam("id") int id) {
-    Person p = persons.get(id);
-    return gson.toJson(p);
-  }
+    @GET
+    @Path("all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getAll() {
+        return gson.toJson(persons.values());
+    }
 
-  
-  @POST
-  @Consumes(MediaType.APPLICATION_JSON)
-  @Produces(MediaType.APPLICATION_JSON)
-  public String putJson(String personAsJson) {
-    Person p = gson.fromJson(personAsJson, Person.class);
-    p.setId(nextId);
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson(@PathParam("id") int id) {
+        Person p = persons.get(id);
+        return gson.toJson(p);
+    }
     
-    persons.put(nextId,p);
-    nextId++;
-    return gson.toJson(p);
-  }
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String updateJson(String personAsJson) {
+        Person p = gson.fromJson(personAsJson, Person.class);
+        persons.put(p.getId(), p);
+        return gson.toJson(p);
+    }
+    
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{id}")
+    public String deleteJson(@PathParam("id") int id) {
+        return gson.toJson(persons.remove(id));
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String putJson(String personAsJson) {
+        Person p = gson.fromJson(personAsJson, Person.class);
+        p.setId(nextId);
+
+        persons.put(nextId, p);
+        nextId++;
+        return gson.toJson(p);
+    }
 }
